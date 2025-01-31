4c22a84303eee0818458b10afc2469f61b29d543
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2025-01-31 15:14:18 +0530

    Update buttons.py

--- SUKOON_MUSIC/utils/stuffs/buttons.py
+++ SUKOON_MUSIC/utils/stuffs/buttons.py
@@ -4,8 +4,8 @@ from pyrogram import Client, filters, enums
 class BUTTONS(object):
     ABUTTON = [
     [
-        InlineKeyboardButton("˹ sᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/PURVI_SUPPORT"),
-        InlineKeyboardButton("˹ ᴜᴘᴅᴧᴛᴇ ˼", url="https://t.me/+gMy8Cp190ediNzZl")
+        InlineKeyboardButton("˹ sᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/ll_Bot_Promotion_ll"),
+        InlineKeyboardButton("˹ ᴜᴘᴅᴧᴛᴇ ˼", url="https://t.me/+dGzBB3OZsfliYmY1")
     ],
     [
         InlineKeyboardButton("˹ sᴏᴜʀᴄᴇ ˼", callback_data="gib_source"),
