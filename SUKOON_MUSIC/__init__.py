4edd14d10c8bab00c035fc931427ae90008c003b
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2025-01-31 12:30:58 +0530

    Add files via upload

--- SUKOON_MUSIC/__init__.py
+++ SUKOON_MUSIC/__init__.py
@@ -1,1 +1,28 @@
+from SUKOON_MUSIC.core.bot import PURVI
+from SUKOON_MUSIC.core.dir import dirr
+from SUKOON_MUSIC.core.git import git
+from SUKOON_MUSIC.core.userbot import Userbot
+from SUKOON_MUSIC.misc import dbb, heroku
 
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = PURVI()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
