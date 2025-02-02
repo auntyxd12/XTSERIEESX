ebc083615d0572a4d12e6b91929441456a07b8b1
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2025-02-03 00:03:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -138,8 +138,8 @@ PING_IMG_URL = getenv(
 )
 PLAYLIST_IMG_URL = "https://files.catbox.moe/i493lf.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/i0qmgf.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/8e3552aa743ffdb6f18c9.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/8e3552aa743ffdb6f18c9.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/qfxxpb.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/qfxxpb.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
