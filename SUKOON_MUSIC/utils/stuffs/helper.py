45c9935792aa6aca146239b542920975672da50e
auntyxd12 <tmxd1523@gmail.com>
2025-02-19 00:38:49 +0530

    Update helper.py

--- SUKOON_MUSIC/utils/stuffs/helper.py
+++ SUKOON_MUSIC/utils/stuffs/helper.py
@@ -33,7 +33,7 @@ class Helper(object):
 │❍ ᴘᴀɪᴅ ᴘʀᴏᴍᴏ + ʙᴏᴛ ʜᴏsᴛɪɴɢ⎚
 ╰───────────────────⦿
 ❍ ᴛɪᴍᴇ ᴡᴀsᴇᴛʀ = ʙʟᴏᴄᴋ 
-❍ ᴄᴏɴᴛᴀᴄᴛ - [ᴄʟɪᴄᴋ ғᴏʀ ᴅᴍ](https://t.me/lll_Oye_Zayn_lll)'''
+❍ ᴄᴏɴᴛᴀᴄᴛ - [ᴄʟɪᴄᴋ ғᴏʀ ᴅᴍ](https://t.me/legend_mickey)'''
     
     HELP_admin = """<b><u>Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Adᴍɪɴ:</u></b>
 
